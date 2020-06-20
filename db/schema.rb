341d7952212f54191b844c3274ed976761fa3ae5
Neil Empey <argus.two.2@gmail.com>
2020-06-20 20:49:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200620204512) do
+ActiveRecord::Schema.define(version: 20200620204913) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20200620204512) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
